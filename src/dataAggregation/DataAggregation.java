22efb5f4c1ae57a64bacc7caf1b65cb6ba0a7d17
sunflower1988 <ThisisSunflowerOo@gmail.com>
2014-12-08 18:40:43 +0800

    aa

--- src/dataAggregation/DataAggregation.java
+++ src/dataAggregation/DataAggregation.java
@@ -9,6 +9,6 @@ public class DataAggregation {
 	public static void main(String[] args) throws FileNotFoundException{
 		DrawGraph dg = new DrawGraph();
 		InitialSolution is = new InitialSolution(dg.DrawG());
-		
+		is.ResultTree();
 	}
 }
--- src/dataAggregation/InitialSolution.java
+++ src/dataAggregation/InitialSolution.java
@@ -13,21 +13,27 @@ class TreeNode{
 }
 public class InitialSolution {
 	int nodenum;
+	VNode[] VN;
 	 InitialSolution(VNode[] V){
-/*		VNode[] VN = new VNode[V.length];
-		VN=V; 							//shallow clone
-*/
-		 nodenum = V.length;
-		 TreeNode root = new TreeNode();
-		 root.data = 0;
-		 root.parent = 0;
-		 root.timeTide = 1;
-		 root.children = null;
+
+		nodenum = V.length;
+		VN = new VNode[nodenum];
+		VN=V; 							//deep clone
+		for(int i=0;i<nodenum;i++){
+			VN[i]=V[i];
+		}
+		
+		TreeNode root = new TreeNode();
+		root.data = VN[0].from;
+		root.parent = 0;
+		root.timeTide = 1;
+		root.children = null;
 	}
 
 	 public TreeNode[] ResultTree(){
 		 System.out.println(nodenum);
 		 TreeNode[] tn = new TreeNode[nodenum];
+		 
 		 return tn;
 	 }
 }
